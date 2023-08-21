7af3b830499c99e2c14ddf5e3d074362d7b41370
Minji-Sohn2 <cyzh1311@gmail.com>
2023-08-21 10:04:11 +0900

    init commit

---
+++ src/test/java/com/example/websocketprac/WebsocketPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websocketprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
